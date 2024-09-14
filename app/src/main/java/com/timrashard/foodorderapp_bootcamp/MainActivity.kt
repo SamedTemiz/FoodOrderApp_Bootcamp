32c244c5378f097d7d02f37e7db0a7d3b90fe1d1
SamedTemiz <samedtemiz99@gmail.com>
2024-09-14 12:54:37 +0300

    Initial commit

---
+++ app/src/main/java/com/timrashard/foodorderapp_bootcamp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.timrashard.foodorderapp_bootcamp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.timrashard.foodorderapp_bootcamp.ui.theme.FoodOrderApp_BootcampTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodOrderApp_BootcampTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodOrderApp_BootcampTheme {
+        Greeting("Android")
+    }
+}
