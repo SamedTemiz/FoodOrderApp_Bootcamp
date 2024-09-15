78f706a8053ab1a4bca0a041c2b173cfc27190cc
SamedTemiz <samedtemiz99@gmail.com>
2024-09-15 15:50:08 +0300

    Dependency and dagger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,4 +69,19 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.5")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.1")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+
+    // Dagger-Hilt Kapt
+    implementation("com.google.dagger:hilt-android:2.49")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
---
+++ app/src/main/java/com/timrashard/foodorderapp_bootcamp/presentation/FoodOrderApp.kt
@@ -0,0 +1,7 @@
+package com.timrashard.foodorderapp_bootcamp.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodOrderApp : Application()
